bb5c93dd55e90bc3f34889e571634208c61bb0fe
Faesalkhan <pathanf9545@gmail.com>
2024-05-19 18:18:03 +0530

    added mouer pointer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import MousePointer from "./components/MousePointer";
 
 const App = () => {
   return (
     <div>
+      <MousePointer />
       <Header />
       <Body />
     </div>
