ae3e6279539a5767b3c2bc24ca6f92ce75c7f597
Faesalkhan <pathanf9545@gmail.com>
2024-05-19 18:17:39 +0530

    aligned the cols of trust rows on large screen

--- src/components/Trust.js
+++ src/components/Trust.js
@@ -6,26 +6,26 @@ import ratings from "../img/motionarteffect-img4.png";
 
 const Trust = () => {
   return (
-    <div className="container py-4">
+    <div className="container-fluid py-4">
       <h4 className="text-white text-center mb-5">
         Trusted by thousands of users around the world
       </h4>
-      <div className="row ">
-        <div className="col-sm-5 col-md-4 col-lg-4 d-flex align-items-center applogocolumn ">
+      <div className="row trust-row">
+        <div className="col-sm-5 col-md-3 col-lg-3 d-flex align-items-center applogocolumn ">
           <img src={app2} className="applogos" />
           <div className="ratingdiv">
             <img src={ratings} className="ratinglogo" />
             <p className="text-white">4.5 Score, 9 Reviews</p>
           </div>
         </div>
-        <div className="col-sm-5 col-md-4 col-lg-4 d-flex align-items-center applogocolumn">
+        <div className="col-sm-5 col-md-3 col-lg-3 d-flex align-items-center applogocolumn">
           <img src={app1} className="applogos" />
           <div className="ratingdiv">
             <img src={ratings} className="ratinglogo" />
             <p className="text-white">4.5 Score, 9 Reviews</p>
           </div>
         </div>
-        <div className="col-sm-5 col-md-4 col-lg-4 d-flex align-items-center applogocolumn">
+        <div className="col-sm-5 col-md-3 col-lg-3 d-flex align-items-center applogocolumn">
           <img src={app3} className="applogos" />
           <div className="ratingdiv">
             <img src={ratings} className="ratinglogo" />
